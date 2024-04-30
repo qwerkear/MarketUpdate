8c2673acdb7038764b6d2629bf056801ee58fe12
Jamie <qwer.kear@gmail.com>
2024-04-30 14:36:43 -0400

    update email body to only include the current month

--- index.mjs
+++ index.mjs
@@ -4,16 +4,19 @@
 import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
 const ses = new SESClient({ region: "us-east-1" });
 import axios from 'axios'
-let {data} = await axios.get('https://api.bls.gov/publicAPI/v2/timeseries/data/CUUR0000SA0L1E', {responseType: 'JSON'})
 
 export const handler = async(event) => {
+  let {data} = await axios.get('https://api.bls.gov/publicAPI/v2/timeseries/data/CUUR0000SA0L1E', {responseType: 'JSON'})
+  const cpiJson = JSON.parse(data);
+  const thisMonthsCPI = JSON.stringify(cpiJson.Results.series[0].data[0])
+
   const command = new SendEmailCommand({
     Destination: {
       ToAddresses: ["qwer.kear@gmail.com"],
     },
     Message: {
       Body: {
-        Text: { Data: data },
+        Text: { Data: thisMonthsCPI },
       },
 
       Subject: { Data: "Test Email" },
